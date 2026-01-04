3c97c305c97e636512d41412bfbf4950975f02be
Alexaner Yarovinsky <48382982+AlexanderY88@users.noreply.github.com>
2026-01-04 22:28:31 +0200

    Products api bug fixes - 400 product exist error

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const joi = require("joi");
-const User = require("../models/Users");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const Product = require("../models/Product");
 
 // joi validation of product
 const checkProductBody = joi.object({
@@ -27,5 +25,34 @@ router.get("/", async (req,res) => {
     }
 });
 
+// product joi validation
+const checkNewProductBody = joi.object({
+    productId: joi.number().required(),
+    name: joi.string().required(),
+    price: joi.number().required(),
+    category: joi.string().required(),
+    description: joi.string().required()
+}); 
+// add new product
+router.post("/", async (req, res) => {
+    try {
+        // check body with joi
+        const { error } = checkNewProductBody.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        // check if product exists by id 
+        let product = await Product.findOne({productId: req.body.productId});
+        if (product) return res.status(400).send(`This product with id ${req.body.productId} already exists.`);
+        // create new product object
+        product = new Product(req.body);
+        await product.save();
+        res.status(201).json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// get product by id
+
+// 
 
 module.exports = router;
